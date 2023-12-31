d5c950070e2a408eed9db9c9aed5a02ec193a3c3
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2023-12-31 22:12:35 +0530

    Sun Dec 31 10:12:35 PM IST 2023

--- leet/easy/count_tested_devices_after_operation.cpp
+++ leet/easy/count_tested_devices_after_operation.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <vector>
 
-int countTestedDevices(std::vector<int>&);
+int countTestedDevices(std::vector<int> &);
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     std::vector<int> a = {1, 1, 2, 1, 3};
     std::cout << countTestedDevices(a) << std::endl;
 }
 
-int countTestedDevices(std::vector<int>& batteryPercentages) {
+int countTestedDevices(std::vector<int> &batteryPercentages) {
     int tested_devices = 0;
     for (int i = 0; i < batteryPercentages.size(); i++) {
         if (batteryPercentages[i] > 0) {
             tested_devices++;
-            for ( int j = i+1; j < batteryPercentages.size(); j++ ) {
+            for (int j = i + 1; j < batteryPercentages.size(); j++) {
                 if (batteryPercentages[j] > 0) {
                     batteryPercentages[j]--;
                 }
             }
-        } else continue;
+        } else
+            continue;
     }
     return tested_devices;
 }
--- test/healthchecker.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-
-int main () {
-    std::cout << "this is a health checker project" << std::endl;
-}
